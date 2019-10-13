57fe47a4e8dd7512aa92c68dd5ecf5d41248393f
Starla Dayton <starla.d606@stu.nebo.edu>
2019-10-12 20:06:27 -0600

    prototype 1

--- creat with code/Prototype 1/Assets/CarCameraFollow.cs
+++ creat with code/Prototype 1/Assets/CarCameraFollow.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -17,9 +16,8 @@ public class CameraFollow : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Offset to the camera behind the player by adding the players posistion 
         transform.position = player.transform.position + offset;
     }
-
-
-    }
+}
 
--- creat with code/Prototype 1/Assets/scripts/PlayerController.cs
+++ creat with code/Prototype 1/Assets/scripts/PlayerController.cs
@@ -3,9 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
-{// added private float
+{
+    // Private variables
     private float speed = 5.0f;
-    private float turnSpeed;
+    private float turnSpeed =25.0f;
     private float horizontalInput;
     private float forwardInput;
 
@@ -16,14 +17,15 @@ public class PlayerController : MonoBehaviour
     }
 
     // Update is called once per frame
-   
-
     void Update()
     {
+        // This is where we get player input
         horizontalInput = Input.GetAxis("Horizontal");
         forwardInput = Input.GetAxis("Vertical");
-        //we'll move the vehicle forward
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
-   
-    }
+       
+        // We move the vehicle forward
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        // We turn the vehicle
+        transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
+    }   
 }
