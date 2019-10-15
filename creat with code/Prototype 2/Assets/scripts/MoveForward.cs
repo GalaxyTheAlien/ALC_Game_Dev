0b18a4993441d460d185b0b968f54713a1190f47
Starla Dayton <starla.d606@stu.nebo.edu>
2019-10-15 10:27:14 -0600

    more

--- creat with code/Prototype 2/Assets/scripts/MoveForward.cs
+++ creat with code/Prototype 2/Assets/scripts/MoveForward.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class MoveForward : MonoBehaviour
 {
+	public float speed = 40;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -12,7 +14,5 @@ public class MoveForward : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {
-        
-    }
+    {transform.Translate(Vector3.forward * Time.deltaTime * speed);}
 }
