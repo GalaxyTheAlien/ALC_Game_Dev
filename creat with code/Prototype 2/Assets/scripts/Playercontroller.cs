6d19c402d6b5e65366e0fe4665d4424978308319
Starla Dayton <starla.d606@stu.nebo.edu>
2019-10-15 10:16:25 -0600

    prototype 2

--- creat with code/Prototype 2/Assets/scripts/Playercontroller.cs
+++ creat with code/Prototype 2/Assets/scripts/Playercontroller.cs
@@ -6,7 +6,7 @@ public class Playercontroller : MonoBehaviour
 {
     public float horizontalInput;
     public float speed = 10.0f;
-
+	public float xRange = 10;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,12 +15,17 @@ public class Playercontroller : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {
-        if (transform.position.x < -10)
-        {
-            transform.position = new Vector3(-10, transform.position.y, transform.position.z);
-        }
-        horizontalInput = Input.GetAxis("Horizontal");
+	{
+		// keep the player in bounds
+		if (transform.position.x < -xRange)
+		{
+			transform.position = new Vector3(-10, transform.position.y, transform.position.z);
+		}
+		if (transform.position.x < -xRange)
+		{
+			transform.position = new Vector3(-10, transform.position.y, transform.position.z);
+		}
+		horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
     }   
 }
