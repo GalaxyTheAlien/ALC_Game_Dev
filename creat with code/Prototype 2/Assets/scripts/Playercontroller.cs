887d3751796e0bfdb234342bf3a8f4472b39b391
Starla Dayton <starla.d606@stu.nebo.edu>
2019-10-15 21:06:10 -0600

    lol

--- creat with code/Prototype 2/Assets/scripts/Playercontroller.cs
+++ creat with code/Prototype 2/Assets/scripts/Playercontroller.cs
@@ -6,6 +6,9 @@ public class Playercontroller : MonoBehaviour
 {
     public float horizontalInput;
     public float speed = 10.0f;
+    public float xRange = 10.0f;
+
+    public GameObject projectilePrefab;
 
     // Start is called before the first frame update
     void Start()
@@ -16,14 +19,14 @@ public class Playercontroller : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (transform.position.x < -10)
+        if (transform.position.x < -xRange)
         {
-            transform.position = new Vector3(-10, transform.position.y, transform.position.z);
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
         }
 
-        if (transform.position.x < -10)
+        if (transform.position.x < xRange)
         {
-            transform.position = new Vector3(-10, transform.position.y, transform.position.z);
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
         }
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
