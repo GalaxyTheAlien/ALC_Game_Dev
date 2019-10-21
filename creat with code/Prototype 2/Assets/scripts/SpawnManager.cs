2097071fda9dbdac827a8469136b684d1a4d5b5f
Starla Dayton <starla.d606@stu.nebo.edu>
2019-10-21 15:18:17 -0600

    SpawnManager

--- creat with code/Prototype 2/Assets/scripts/SpawnManager.cs
+++ creat with code/Prototype 2/Assets/scripts/SpawnManager.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     public GameObject[] animalPrefabs; 
-
     // Start is called before the first frame update
     void Start()
     {
@@ -16,10 +15,8 @@ public class SpawnManager : MonoBehaviour
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.S))
-        {
-            int animalIndex = Random.Range(0, 3);
-
-            Instantiate(animalPrefabs[animalIndex], new Vector3(0, 0, 20), animalPrefabs[animalIndex].transform.rotation);
+        { 
+            Instantiate(animalPrefabs[0], new Vector3(0, 0, 20), animalPrefabs[0].transform.rotation);
         }
     }
 }
