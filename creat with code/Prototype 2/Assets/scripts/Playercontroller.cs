35aae2aa80384871f291942a40dc1a63c357a6ad
Starla Dayton <starla.d606@stu.nebo.edu>
2019-10-20 18:43:19 -0600

    yo i did it

--- creat with code/Prototype 2/Assets/scripts/Playercontroller.cs
+++ creat with code/Prototype 2/Assets/scripts/Playercontroller.cs
@@ -28,7 +28,13 @@ public class Playercontroller : MonoBehaviour
         {
             transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
         }
+
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
-    }   
-}
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+        }
+      }
+    }
