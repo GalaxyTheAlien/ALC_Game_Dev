95e9757a869bb5b56dc32a54a66d1fdda98df236
Starla Dayton <starla.d606@stu.nebo.edu>
2019-10-28 17:47:26 -0600

    Auto stash before merge of "master" and "origin/master"
    code

--- creat with code/Prototype 2/Assets/scripts/SpawnManager.cs
+++ creat with code/Prototype 2/Assets/scripts/SpawnManager.cs
@@ -5,7 +5,11 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     public GameObject[] animalPrefabs;
+<<<<<<< Updated upstream
     public int animalIndex;
+=======
+
+>>>>>>> Stashed changes
     // Start is called before the first frame update
     void Start()
     {
@@ -13,10 +17,20 @@ public class SpawnManager : MonoBehaviour
     }
 
     // Update is called once per frame
+<<<<<<< Updated upstream
     void Update(){
         if (Input.GetKeyDown(KeyCode.S)) { 
          Instantiate(animalPrefabs[animalIndex], new Vector3(0, 0, 20),
          animalPrefabs[animalIndex].transform.rotation);
+=======
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            int animalindex = Random.Range(0, 3);
+
+            Instantiate(animalPrefabs[animalindex], new Vector3(0, 0, 20), animalPrefabs[animalindex].transform.rotation);
+>>>>>>> Stashed changes
         }
     }
 }
