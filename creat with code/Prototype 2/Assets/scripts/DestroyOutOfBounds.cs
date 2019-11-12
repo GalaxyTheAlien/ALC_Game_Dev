858c9039d579523fbfefa3dd0fc6f4f9e4ae9986
Starla Dayton <starla.d606@stu.nebo.edu>
2019-11-12 10:26:23 -0700

    More stuff

--- creat with code/Prototype 2/Assets/scripts/DestroyOutOfBounds.cs
+++ creat with code/Prototype 2/Assets/scripts/DestroyOutOfBounds.cs
@@ -6,6 +6,7 @@ public class DestroyOutOfBounds : MonoBehaviour
 {
     private float topBound = 30;
     private float lowerBound = -10;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,11 +16,13 @@ public class DestroyOutOfBounds : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // If an object goes past the players veiw in the game, remove that object
         if (transform.position.z > topBound)
         {
             Destroy(gameObject);  
         } else if (transform.position.z < lowerBound)
         {
+            Debug.Log("Game Over");
             Destroy(gameObject);
         } 
     }
