0240d92e0c246359459d08d2bf0a5b88ab668e78
Starla Dayton <starla.d606@stu.nebo.edu>
2019-11-20 10:28:48 -0700

    new

--- creat with code/Prototype 2/Assets/Challenge 2/Scripts/SpawnManagerX.cs
+++ creat with code/Prototype 2/Assets/Challenge 2/Scripts/SpawnManagerX.cs
@@ -24,7 +24,7 @@ public class SpawnManagerX : MonoBehaviour
         Vector3 spawnPos = new Vector3(Random.Range(spawnLimitXLeft, spawnLimitXRight), spawnPosY, 0);
 
         // instantiate ball at random spawn location
-        Instantiate(ballPrefabs[0], spawnPos, ballPrefabs[0].transform.rotation);
+        Instantiate(ballPrefabs[0], spawnPos, ballPrefabs[0].transform.rotation) ;
     }
 
 }
